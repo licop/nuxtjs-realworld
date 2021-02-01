7775d98c9d947162cfd51330e97a46c09e680fac
licop <licop@users.noreply.github.com>
2021-02-01 19:26:17 +0800

    update login

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -8,22 +8,23 @@
           <p class="text-xs-center">
             <nuxt-link v-if="isLogin" to="/register">Need an account?</nuxt-link>
             <nuxt-link v-else to="/login">Have an account?</nuxt-link>
-
           </p>
 
           <ul class="error-messages">
-            <li>That email is already taken</li>
+            <template v-for="(messages, filed) in errors">
+              <li v-for="(message, index) in messages" :key="index">{{ filed }} {{ message }} </li>
+            </template>
           </ul>
 
-          <form>
+          <form @submit.prevent="onSubmit">
             <fieldset class="form-group" v-if="!isLogin">
-              <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+              <input v-model = "user.username" class="form-control form-control-lg" type="text" placeholder="Your Name" required>
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Email">
+              <input v-model = "user.email" class="form-control form-control-lg" type="email" placeholder="Email" required>
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="password" placeholder="Password">
+              <input v-model = "user.password" class="form-control form-control-lg" type="password" placeholder="Password" required minlength="8">
             </fieldset>
             <button class="btn btn-lg btn-primary pull-xs-right">
               {{ isLogin ? 'Sign in' : 'Sign up' }}
@@ -37,11 +38,38 @@
 </template>
 
 <script>
+import { login, register } from '@/api/user'
+
 export default {
   name: 'LoginIndex',
   computed: {
     isLogin () {
       return this.$route.name === 'login'
+    }
+  },
+  data () {
+    return {
+      user: {
+        username: '',
+        email: '',
+        password: ''
+      },
+      errors: {}
+    }
+  },
+  methods: {
+    async onSubmit () {
+      try {
+        // 表单提交请求登录
+        const { data } = this.isLogin ? await login({
+          user: this.user
+        }) : await register({
+          user: this.user
+        })
+        this.$router.push('/')
+      } catch (error) {
+        this.errors = error.response.data.errors        
+      }
     }
   }
 }
